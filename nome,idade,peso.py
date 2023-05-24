c9af4c931b621bc887e6d37c61cccccf778441ef
naomiikedo <125026741+naomiikedo@users.noreply.github.com>
2023-05-24 18:33:10 +0900

    Update nome,idade,peso.py

--- nome,idade,peso.py
+++ nome,idade,peso.py
@@ -1,4 +1,4 @@
 nome=input('qual o seu nome:')
 idade=input('qual a sua idade?')
 peso=input('qual o seu peso?')
-print('nome:',nome,' idade:',idade,' peso:',peso)
+print('NOME:{} IDADE:{} PESO:{}'.format(nome, idade, peso))
